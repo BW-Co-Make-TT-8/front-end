0a4a22e605af517db08c5a3ec4b59f4d07c9189e
david chang <bjchang@Davids-MacBook-Air.local>
2021-01-09 11:59:59 -0800

    added logout functionality

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,7 @@ import About from './Home/about-us';
 import MeetTheTeam from './Home/meet-the-team';
 import Profile from './User/profile';
 import Post from './User/Posts/post';
+import Logout from './Logout';
 
 // Will be rendering the home page here as the default but am setting up the company logo as the header so that it is set on all lower level pages.
 
@@ -21,6 +22,7 @@ function App() {
       </header>
       <Route path='/signup' component={Signup} />
       <Route path='/login' component={Login} />
+      <Route path='/logout' component={Logout}/>
       <Route exact path='/' component={Home} />
       <Route path='/about' component={About} />
       <Route path='/team' component={MeetTheTeam} />
