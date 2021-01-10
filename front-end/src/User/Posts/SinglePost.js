18a165000466a1643687eff1fbef8ba9af6638fb
seth-bradshaw <seth.bradshaw27@gmail.com>
2021-01-09 17:58:28 -0700

    got most of comment functionality done. Just won't post

--- front-end/src/User/Posts/SinglePost.js
+++ front-end/src/User/Posts/SinglePost.js
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 import { axiosWithAuth } from '../../Utils/axiosWithAuth';
 import Comments from '../Comments/Comments';
-import axios from 'axios';
 
-export default function PostDetails() {
+export default function SinglePost() {
     const { postid } = useParams()
-    const [currentPost, setCurrentPost] = useState(null);
+    const [currentPost, setCurrentPost] = useState({});
 
     useEffect(() => {
-        console.log("USE EFFECT IS WORKING")
         axiosWithAuth()
             .get(`/posts/${postid}`)
             .then(res => {
@@ -22,16 +20,10 @@ export default function PostDetails() {
 
     return (
         <>
-        {
-            <h2>YOU ARE HERE MOTHER FUCKER</h2>
-        }
             {
                 currentPost.imgurl != null ?
                 <div className='single-post-container-with-img'>
                     <h2>{currentPost.title}</h2>
-                    {
-                    console.log("YOU ARE HERE AND TERNARY WORKS RIGHT===> ", currentPost)
-                    }
                     <img src={currentPost.imgurl}></img>
                     <div>
                         <p>{currentPost.city} {currentPost.state}, {currentPost.location}</p>
@@ -39,21 +31,20 @@ export default function PostDetails() {
                     <p>{currentPost.postbody}</p>
                     <div className="comment-container">
                         {
-                            currentPost.comments != null ? <Comments postId={currentPost.postid} /> : null
+                            currentPost.comments != null ? <Comments comments={currentPost.comments} /> : null
                         }
                     </div>
                 </div> 
                 :
                 <div className='single-post-container-without-img'>
                     <h2>{currentPost.title}</h2>
-                    
                     <div>
                         <p>{currentPost.city} {currentPost.state}, {currentPost.location}</p>
                     </div>
                     <p>{currentPost.postbody}</p>
                     <div className="comment-container">
                         {
-                            currentPost.comments != null ? <Comments postId={currentPost.postid} /> : null
+                            currentPost.comments != null ? <Comments comments={currentPost.comments} /> : null
                         }
                     </div>
                 </div>
